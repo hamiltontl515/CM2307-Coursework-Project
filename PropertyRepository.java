14cbd9549041350780a3c397da37503a9fcbbed9
hamiltontl515 <hamiltontl@cardiff.ac.uk>
2026-01-01 19:05:27 +0000

    search properties function added

--- PropertyRepository.java
+++ PropertyRepository.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.time.LocalDate;
 
 public class PropertyRepository{
     private HashMap<String, Property> propertiesID = new HashMap<>();
@@ -9,5 +10,35 @@ public class PropertyRepository{
 
     public Property getPropertyByID(String propertyID){
         return propertiesID.get(propertyID);
+    }
+
+    public List<Room> roomSearch(String university, LocalDate start, LocalDate end, Double pricePerWeek){
+
+        List<Room> searchResult = new ArrayList<>();
+
+        Boolean searchUniveristy = true;
+
+        for(Property property: propertiesID.values()){
+            if(university != null && property.getUniversity() != university){
+                searchUniveristy = false;
+            }
+
+            if(searchUniveristy){
+                for(Room room: property.getRooms()){
+                    Boolean returnRoom = true;
+
+                    if((start != null && end != null) && !(room.checkAvailability(start, end))){
+                        returnRoom = false;
+                    }
+                    if(pricePerWeek != null && pricePerWeek > room.getPricePerWeek()){
+                        returnRoom = false;
+                    }
+                    if(returnRoom){
+                        searchResult.add(room);
+                    }
+                }
+            }
+        }
+        return searchResult;
     }
 }
