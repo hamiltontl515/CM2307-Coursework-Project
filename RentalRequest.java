75abe48af05256bbbbb9d6d7f69c1e22e65e295c
hamiltontl515 <hamiltontl@cardiff.ac.uk>
2026-01-02 16:20:01 +0000

    rental request added

---
+++ RentalRequest.java
@@ -0,0 +1,66 @@
+import java.util.*;
+import java.time.LocalDate;
+
+public class RentalRequest{
+    private String requestID;
+    private Room requestRoom;
+    private Student requestStudent;
+    private RentalRequestStatus requestStatus;
+    private BookingSlot requestSlot;
+
+    public RentalRequest(String requestID, Room requestRoom, Student requestStudent, BookingSlot requestSlot){
+        this.requestID = requestID;
+        this.requestRoom = requestRoom;
+        this.requestStudent = requestStudent;
+        this.requestSlot = requestSlot;
+        this.requestStatus = RentalRequestStatus.REQUESTED;
+
+    }
+
+    //getters
+    public String getRequestID(){
+        return requestID;
+    }
+    public Room getRequestRoom(){
+        return requestRoom;
+    }
+    public Student getRequestStudent(){
+        return requestStudent;
+    }
+    public RentalRequestStatus getRentalRequestStatus(){
+        return requestStatus;
+    }
+    public BookingSlot getRentalRequestBookingSlot(){
+        return requestSlot;
+    }
+
+    //setters
+    public void setRequestID(String newRequestID){
+        requestID = newRequestID;
+    }
+    public void setRequestRoom(Room newRequestRoom){
+        requestRoom = newRequestRoom;
+    }
+    public void setRequestStudent(Student newRequestStudent){
+        requestStudent = newRequestStudent;
+    }
+    public void setRequestBookingSlot(BookingSlot newRequestSlot){
+        requestSlot = newRequestSlot;
+    }
+
+    public void acceptRequest(){
+        if(requestStatus == RentalRequestStatus.REQUESTED){
+            requestStatus = RentalRequestStatus.ACCEPTED;
+        }else{
+            System.out.println("Request status already accepted/denied");
+        }
+    }
+    public void denyRequest(){
+        if(requestStatus == RentalRequestStatus.REQUESTED){
+            requestStatus = RentalRequestStatus.DENIED;
+        }else{
+            System.out.println("Request status already accepted/denied");
+        }
+    }
+
+}
